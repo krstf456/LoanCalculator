1ab7116f169ef7030afb28131acb9cd306a8ea84
Kristóf <krstf456@gmail.com>
2020-05-17 17:21:19 +0200

    working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Button from './components/button'
-import MonthlyCost from './components/monthlyCost'
 
-import Repayment from './components/repayment'
 import Input from './components/input'
 
 
